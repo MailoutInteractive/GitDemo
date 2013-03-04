3a12319c49171258b3341f81fc3e82a6ff652137
Mike Bridge <mike@mailoutinteractive.com>
2013-03-04 09:25:35 -0700

    Initial Commit

---
+++ MvcApplication.Tests/Utils/DemoTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MvcApplication.Utils;
+using NUnit.Framework;
+
+namespace MvcApplication.Tests.Utils
+{
+    [TestFixture]
+    public class DemoTests
+    {
+        [Test]
+        public void It_Should_Add_Two_Integers()
+        {
+            // Arrange
+            int x = 2, y = 3;
+            Demo demo = new Demo();
+            
+            // Act
+            int result = demo.Add(x, y);
+            
+            // Assert
+            Assert.That(result, Is.EqualTo(x + y));
+
+        }
+    }
+}
---
+++ MvcApplication/Utils/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcApplication.Utils
+{
+    public class Demo
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
